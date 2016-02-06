09bed7a7a78751a2fa4981ae2e9de603fb9d85d0
Milos Simic <milossimicsimo@gmail.com>
2016-02-06 11:28:17 +0100

    Simple Network monitor in c#

---
+++ NetworkConnection/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NetworkConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPisi = new System.Windows.Forms.Label();
+            this.tmTime = new System.Windows.Forms.Timer(this.components);
+            this.zg1 = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblPisi
+            // 
+            this.lblPisi.AutoSize = true;
+            this.lblPisi.Location = new System.Drawing.Point(80, 22);
+            this.lblPisi.Name = "lblPisi";
+            this.lblPisi.Size = new System.Drawing.Size(57, 13);
+            this.lblPisi.TabIndex = 0;
+            this.lblPisi.Text = "Interfaces:";
+            // 
+            // tmTime
+            // 
+            this.tmTime.Interval = 1000;
+            this.tmTime.Tick += new System.EventHandler(this.tmTime_Tick);
+            // 
+            // zg1
+            // 
+            this.zg1.EditButtons = System.Windows.Forms.MouseButtons.Left;
+            this.zg1.Location = new System.Drawing.Point(83, 82);
+            this.zg1.Name = "zg1";
+            this.zg1.PanModifierKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.None)));
+            this.zg1.ScrollGrace = 0D;
+            this.zg1.ScrollMaxX = 0D;
+            this.zg1.ScrollMaxY = 0D;
+            this.zg1.ScrollMaxY2 = 0D;
+            this.zg1.ScrollMinX = 0D;
+            this.zg1.ScrollMinY = 0D;
+            this.zg1.ScrollMinY2 = 0D;
+            this.zg1.Size = new System.Drawing.Size(499, 333);
+            this.zg1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(143, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(439, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 437);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zg1);
+            this.Controls.Add(this.lblPisi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPisi;
+        private System.Windows.Forms.Timer tmTime;
+        private ZedGraph.ZedGraphControl zg1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ NetworkConnection/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms.DataVisualization.Charting;
+using ZedGraph;
+
+namespace NetworkConnection
+{
+    public partial class Form1 : Form
+    {
+        protected NetworkInterface[] nics = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+        protected List<String> names;
+        protected List<long> values;
+        protected int num = 0;
+        protected int index = 0;
+
+        protected PointPairList list = new PointPairList();
+       
+        public Form1()
+        {
+            InitializeComponent();
+            names = new List<string>();
+            values = new List<long>();
+
+            foreach(NetworkInterface n in nics){
+                comboBox1.Items.Add(n.Name);
+            }
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tmTime.Enabled = false;
+            tmTime.Stop();
+
+            ComboBox cb = (ComboBox)sender;
+            index = cb.SelectedIndex;
+            num = 0;
+            list.Clear();
+
+            tmTime.Enabled = true;
+            tmTime.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmTime.Start();
+        }
+
+        private void tmTime_Tick(object sender, EventArgs e)
+        {
+
+            // Get a reference to the GraphPane instance in the ZedGraphControl
+            GraphPane myPane = zg1.GraphPane;
+
+            // Set the titles and axis labels
+            myPane.Title.Text = "Graph of connections speed";
+            myPane.XAxis.Title.Text = "Seconds [S]";
+            myPane.YAxis.Title.Text = "Speed Bytes [B]";
+            myPane.Legend.IsVisible = false;
+
+            // Add gridlines to the plot, and make them gray
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane.XAxis.MajorGrid.Color = Color.LightGray;
+            myPane.YAxis.MajorGrid.Color = Color.LightGray;
+
+            if(tmTime.Enabled){
+                
+                //lblPisi.Text = nics[0].Name + "[ " + nics[0].GetIPv4Statistics().BytesReceived + " ]";
+
+                num++;
+
+                double x = (double)num;
+                double y = (double)nics[0].GetIPv4Statistics().BytesReceived;
+                
+                list.Add(x,y);
+
+            }
+            
+            // Generate a red curve with diamond symbols, and "Alpha" in the legend
+            LineItem myCurve = myPane.AddCurve(comboBox1.SelectedText, list, Color.Red, SymbolType.Circle);
+            
+            // Fill the symbols with white
+            myCurve.Symbol.Fill = new Fill(Color.White);
+
+            //fill the area
+            myCurve.Line.Fill = new Fill(Color.White, Color.Red, 45F);
+
+            // Fill the axis background with a gradient
+            myPane.Chart.Fill = new Fill(Color.White, Color.LightGray, 45.0f);
+
+            // turn off the opposite tics so the Y tics don't show up on the Y2 axis
+            myPane.YAxis.MajorTic.IsOpposite = false;
+            myPane.YAxis.MinorTic.IsOpposite = false;
+            // Don't display the Y zero line
+            myPane.YAxis.MajorGrid.IsZeroLine = false;
+            // Align the Y axis labels so they are flush to the axis
+            myPane.YAxis.Scale.Align = AlignP.Inside;
+
+            // Enable scrollbars if needed
+            zg1.IsShowHScrollBar = true;
+            zg1.IsShowVScrollBar = true;
+            zg1.IsAutoScrollRange = true;
+            zg1.IsScrollY2 = true;
+
+            zg1.AxisChange();
+            // Make sure the Graph gets redrawn
+            zg1.Invalidate();
+            
+        }
+    }
+}
